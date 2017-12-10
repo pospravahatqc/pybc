7638c337f197c1c9dee017c58814e6d128e6f5c3
Stas T <atqcpospravah@gmail.com>
2017-12-10 21:20:56 +0200

    log decorator --> func names + args decorator

--- app_code/decorator_app.py
+++ app_code/decorator_app.py
@@ -1,7 +1,21 @@
-def arguments_log(func):
-    def wrapper(*args):
-        for a in args:
-            print 'arg: "{}"'.format(a)
-        return func(*args)
+from fibonachi_app import fibn
+from pairs_of_number import arnums
 
+# 5. Decorator
+# Decorate your programs with a decorator which prints the function arguments in the func_name(args) format (for instance fib(6).
+
+def decofull(function_to_decorate):
+    def wrapper(*args, **kwargs):
+        print(function_to_decorate.func_name +": "),
+        print(args)
+        print(kwargs)
+        function_to_decorate(*args, **kwargs)
     return wrapper
+
+decofull(321)
+
+fib_decorated = decofull(fibn)
+fib_decorated(7)
+
+arnums_decorated = decofull(arnums)
+arnums_decorated(1,2,3,8,9)
